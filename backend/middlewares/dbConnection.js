78a3b8b5c613a5ae06bce0827817df6a8da72a6a
dhruvinfo28 <dhruv.info28@gmail.com>
2023-04-09 15:49:01 +0530

    models and file_structure

--- backend/middlewares/dbConnection.js
+++ backend/middlewares/dbConnection.js
@@ -1,8 +1,10 @@
+require("dotenv").config()
 const mongoose = require("mongoose")
-const MONGO_URI = ``;
+const MONGO_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.jsenic9.mongodb.net/?retryWrites=true&w=majority`;
 const connectToDb = async () => {
     try {
         await mongoose.connect(MONGO_URI);
+        console.log("DB connected successfully")
     } catch(connectionError) {
         console.error(connectionError)
     }
--- backend/models/termIdMapping.model.js
+++ backend/models/termIdMapping.model.js
@@ -1,1 +1,16 @@
-//TO-DO
+let { Schema, model } = require("mongoose")
+
+let termIdMappingSchema = new Schema({
+    term_name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    term_id: {
+        type: Number,
+        required: true,
+        unique: true
+    }
+})
+
+module.exports = model('TermIdMapping', termIdMappingSchema);

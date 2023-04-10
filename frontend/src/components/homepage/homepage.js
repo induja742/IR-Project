027d40e4025447983cbe24d470d062794bec7835
Induja Kala <indujakala19@gmail.com>
2023-04-10 22:34:00 +0530

    Landing page

--- frontend/src/components/homepage/homepage.js
+++ frontend/src/components/homepage/homepage.js
@@ -1,1 +1,36 @@
+import React from "react";
+import "./homepage.css";
 
+
+const Homepage = () => {
+
+  return (
+    <>
+      <div className="body-background">
+        <div className="xyz">
+          <div className="search-bar">
+            {/* <button className="button-styling">Recall</button>
+            <button className="button-styling">Boolean</button>
+            <button className="button-styling">Retreival</button> */}
+            <button className="button-styling">Design systems</button>
+            <button className="button-styling">Tokenization</button>
+            <button className="button-styling">Normalization</button>
+            <button className="button-styling">Biword</button>
+            <button className="button-styling">Stemming</button>
+            <button className="button-styling">Hash Tables</button>
+            <button className="button-styling">Soundex</button>
+            <button className="button-styling">Explore</button>
+          </div>
+          <h1>What are you looking for?</h1>
+          <input
+            className="home-body-input"
+            type="text"
+            placeholder="Search here"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Homepage;
